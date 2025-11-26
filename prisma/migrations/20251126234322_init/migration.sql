a8f6a342bf994786246342feae7e2d0dc967d4f1
Samuel Barubiriza <samuel.barubiriza@abtatsy.com>
2025-11-27 00:46:15 +0100

    firts minimum setup for db

---
+++ prisma/migrations/20251126234322_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
